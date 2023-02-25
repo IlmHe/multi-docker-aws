e9e60ee2f7ba6bc0c375f4afc0cd30ade0ea7c26
IlmHe <ilmar.r.heinonen@gmail.com>
2023-02-25 19:57:11 +0200

    using gha to push to dockerhub

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,3 +1,4 @@
-import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-it('should render without errors', () => {});
+test('renders learn react link', () => {});
